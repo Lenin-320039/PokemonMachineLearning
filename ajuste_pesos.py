403697837bc2e665ada0c005ec43bda8650c7256
Lenin-320039 <tic-320039@utnay.edu.mx>
2026-01-27 20:40:01 -0600

    Update weight adjustment method in ajuste_pesos.py
    Refactor weight update logic to use map function.

--- ajuste_pesos.py
+++ ajuste_pesos.py
@@ -1,4 +1,7 @@
-for j in range(2):
-    w[j] = w[j] + alpha * grad[j]
+import values_xywalpha as values
 
-print("nuevos pesos w =", w)
+def actualizar_pesos(grad, alpha):
+    # Nueva experiencia = vieja experiencia + (aprendizaje * gradiente)
+    # Usamos map para actualizar ambos pesos a la vez sin for
+    w_nuevos = list(map(lambda w_v, g: w_v + (alpha * g), values.w, grad))
+    return w_nuevos
