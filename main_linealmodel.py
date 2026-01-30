f1ca21af5b269cc681673d7b29d992790acb44fc
Lenin-320039 <tic-320039@utnay.edu.mx>
2026-01-29 18:57:52 -0600

    Refactor linear model with new error handling and imports

--- main_linealmodel.py
+++ main_linealmodel.py
@@ -1,8 +1,10 @@
 import values_xywalpha as values
 import y_predictiva as yp
+import errortotal as errort
 import ajuste_transpuesta as at
 import ajuste_pesos as ap
-
+import nueva_prediccion as np
+import nuevo_vectorerror_errortotal as nve
 # 1. Definimos Alpha (Tasa de aprendizaje)
 alpha = 0.01
 
@@ -11,17 +13,21 @@ y_hat = yp.y_hat
 e = list(map(lambda r, p: r - p, values.y_real, y_hat))
 
 #3 Calculamos el Gradiente usando el módulo de transpuesta
-gradiente = at.calcular_gradiente(e)
+gradiente = at.grad
 
-# 4. Ajustamos los Pesos usando el módulo de ajuste
-nuevos_pesos = ap.actualizar_pesos(gradiente, alpha)
+
+
 
 # SALIDA DE DATOS 
 print("   REPORTE DE ENTRENAMIENTO ARCANINE      ")
-print(f"Predicciones (y_hat):   {y_hat}")
-print(f"Vector de Error (e):    {e}")
-print(f"Gradiente (X^T e):      {gradiente}")
-print(f"Pesos Originales (w):   {values.w}")
-print(f"NUEVOS PESOS (w):       {nuevos_pesos}")
+print(f"1. Error Inicial:       {errort.ErrorTotal1}")
+print(f"2. Gradiente (X^T e):   {at.grad}")
+print(f"3. NUEVOS PESOS (w):    {ap.w_nuevos}")
+print(f"4. Nueva Predicción:    {np.y_hat_nueva}")
+print(f"5. Nuevo Vector Error:  {nve.e_nuevo}")
+print(f"6. NUEVO ERROR TOTAL:   {nve.ErrorTotal_nuevo}")
 
-
+if nve.ErrorTotal_nuevo < errort.ErrorTotal1:
+    print("¡RESULTADO: Arcanine ha mejorado su precisión!")
+else:
+    print("RESULTADO: El error no bajó. Revisa el valor de alpha.")
