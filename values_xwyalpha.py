e3c8e48167b66d29ccb1bc6174541ffd200e4a18
Lenin-320039 <tic-320039@utnay.edu.mx>
2026-01-29 19:10:19 -0600

    Update values_xwyalpha.py

--- values_xwyalpha.py
+++ values_xwyalpha.py
@@ -1,12 +1,31 @@
-
-#! Estos valores son lo correspondiente a las habilidad del pokémon
-X = [
-    [2, 5],
-    [4, 6],
-    [6, 7]
+# X--> Habilidades ... valores entrada
+x = [
+    [2,5], #fila1
+    [4,6], #fila2
+    [6,7] #fila3
 ]
 
-y = [11, 18, 25]    # La experiencia actual de sus habilidades
-w = [1.0, 1.0]      # El nivel actual del Pokemon
-alpha = 0.01        # Que tan rapido aprendera segun su naturaleza
+# W--> Experiencia_NivelActual
+w = [1.1, 1.0]
 
+# Y_real--> Experiencias previas en combate..justifico estos valores
+y_real = [11, 18, 25]
+
+alpha = 0.01
+#2 DEFINIMOS LA FUNCION
+def predecir_fila(fila):
+    return sum(map(lambda val, peso: val * peso, fila, w))
+
+#3 AL FINAL HACEMOS EL CALCULO
+y_hat = list(map(predecir_fila, x))
+
+#4 Mostrar resultados
+print(f"predicciones: {y_hat}")
+
+#FUNCION DE COSTO
+def calcular_error(reales, predichos):
+    n = len(reales)
+    diff_sq = map(lambda r, p: (r - p) ** 2, reales, predichos)
+    return sum(diff_sq) / n
+
+print(f"Error total: {calcular_error(y_real, y_hat)}")
